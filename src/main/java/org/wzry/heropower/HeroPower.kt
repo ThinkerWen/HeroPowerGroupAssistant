d5d94a654eb03f0985f27a770dfe4cc609f439e7
Raptor-wxw <296854007@qq.com>
2022-07-30 17:50:14 +0800

    Fix bug update to 2.2.0

---
+++ src/main/java/org/wzry/heropower/HeroPower.kt
@@ -0,0 +1,33 @@
+package org.wzry.heropower
+
+
+import net.mamoe.mirai.console.plugin.jvm.*
+import net.mamoe.mirai.event.GlobalEventChannel
+import org.wzry.heropower.config.Config
+
+/**
+ * Copyright (c) 2022. Jason Wang (wxw126mail@126.com)
+ * Title: Test
+ * Description:
+ * @author: 王晓文
+ * @date: 2022/7/30 17:28
+ */
+
+
+object HeroPower : KotlinPlugin(
+
+    JvmPluginDescriptionBuilder("org.wzry.heropower.plugin", "2.2.0")
+        .name("王者战力查询")
+        .info("联系作者VX：Rem_wife\n关注公众号：夜寒信息")
+        .author("暮至夜寒")
+        .build()
+) {
+
+    override fun onEnable() {
+        Config.reload()
+        logger.info("王者战力查询 For QQGroup 已启用")
+        val globalListener = GlobalListener.INSTANCE
+        val eventChannel = GlobalEventChannel.parentScope(this)
+        globalListener.listen(eventChannel)
+    }
+}
